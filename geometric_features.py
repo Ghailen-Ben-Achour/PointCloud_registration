27f9beb25b7e761d5feace8288a510566350b9db
Ghailen <benachourghailen@gmail.com>
2021-07-10 17:38:13 +0100

    extract geometric features and visualize

---
+++ geometric_features.py
@@ -0,0 +1,73 @@
+import open3d as o3d
+import numpy as np
+import copy
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser('Model')
+    parser.add_argument('--voxel_size', type=float, default=0.05, help='size of the voxel to downsample')
+
+    return parser.parse_args()
+voxel_size = 0.05  # means 5cm for this dataset
+
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp],
+                                      zoom=0.4559,
+                                      front=[0.6452, -0.3036, -0.7011],
+                                      lookat=[1.9892, 2.0208, 1.8945],
+                                      up=[-0.2779, -0.9482, 0.1556])
+
+
+def preprocess_point_cloud(pcd, voxel_size):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
+
+
+def prepare_dataset(voxel_size):
+    print(":: Load two point clouds and disturb initial pose.")
+    source = o3d.io.read_point_cloud("./ply/cloud_bin_0.pcd")
+    target = o3d.io.read_point_cloud("./ply/cloud_bin_1.pcd")
+    trans_init = np.asarray([[0.0, 0.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0],
+                             [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
+    source.transform(trans_init)
+    draw_registration_result(source, target, np.identity(4))
+
+    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    return source, target, source_down, target_down, source_fpfh, target_fpfh
+
+def prepare_dataset_v2(voxel_size):
+    print(":: Load two point clouds and disturb initial pose.")
+    source = o3d.io.read_point_cloud("./ply/cloud_bin_0.pcd")
+    target = o3d.io.read_point_cloud("./ply/cloud_bin_1.pcd")
+    trans_init = np.asarray([[0.0, 0.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0],
+                             [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
+    source.transform(trans_init)
+    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    return source, target, source_down, target_down, source_fpfh, target_fpfh
+
+if __name__ == '__main__':
+    args = parse_args()
+    voxel_size = args.voxel_size
+    source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset(
+    voxel_size)
+
+
